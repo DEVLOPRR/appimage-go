805ed066e57c050b3e3f1a20b051bf775aeb9bb0
Aditya Mishra <75035219+DEVLOPRR@users.noreply.github.com>
2022-03-28 18:30:26 +0530

    Test File For Testing

---
+++ test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"appimagego/src"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// myAppImage, err := appimagego.NewAppImage("/home/adityam/Documents/appimage-go/LiteXL.AppImage")
+	myAppImage, err := appimagego.NewAppImage("/home/adityam/Applications/bread-0.5.0-x86_64.AppImage")
+	if err != nil {
+		fmt.Println(err)
+		return;
+	}
+
+	// myAppImage.Description
+	fmt.Println("Name:", myAppImage.Name)
+	fmt.Println("Description:", myAppImage.Description)
+	fmt.Println("Version:", myAppImage.Version)
+	fmt.Println("Categories:", strings.Join(myAppImage.Categories, ", "))
+	fmt.Println("Mime Type:", strings.Join(myAppImage.MimeType, ", "))
+	updateInfo, err := myAppImage.GetUpdateInformation();
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println("Update Info:", updateInfo)
+	fmt.Println("AppImage Type:", myAppImage.Type())
+}
